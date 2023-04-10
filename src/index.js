226da6c53c0b79dd7d4d4f0ad9580d1dd0172d20
alisaromantsova <113140050+alisaromantsova@users.noreply.github.com>
2023-04-10 20:48:25 +0300

    1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import { Profile } from 'components/Profile/Profile';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
